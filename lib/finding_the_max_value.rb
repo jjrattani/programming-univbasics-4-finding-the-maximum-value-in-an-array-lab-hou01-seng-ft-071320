890146aec1b5a6d9a26c61f08f049e31ccbf6e2f
Jessica  Rattani <jjrattani@gmail.com>
2020-06-30 01:42:45 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,16 +1,1 @@
-def find_max_value(array)
- counter = 0
- max_value = array.first
- while(array[counter]) do
-  if array[counter] > max_value
-    max_value = array[counter]
-  end
-  counter +=1
-  end
-  max_value
-end
 
-#find the max value from the array
-#assume the first # is 50 for example and compare the next elements to it 
-# ex: if 51 is the next # then replace the 50 with the 51
-#then 51 is the biggest # --> continue to compare until you find the biggest # in the array. 
